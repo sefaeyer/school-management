db5b12baa12c824c04eaba16583a2c3d2db4894c
Sefa Eyer <eyersefa@gmail.com>
2024-11-27 20:00:00 +0300

    entities created

--- src/main/java/com/project/entity/concretes/user/User.java
+++ src/main/java/com/project/entity/concretes/user/User.java
@@ -1,12 +1,19 @@
 package com.project.entity.concretes.user;
 
+
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.project.entity.concretes.business.LessonProgram;
+import com.project.entity.concretes.business.Meet;
+import com.project.entity.concretes.business.StudentInfo;
 import com.project.entity.enums.Gender;
 import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "t_user")
@@ -58,13 +65,35 @@ public class User {
 
     private Boolean isAdvisor;
 
-    private Long advisorTeacherId;
+    private Long advisorTeacherId; // bu Ogrenciler icin lazim, kendi rehber ogretmeninin
+    // id si buraya yazilacak
 
     @Enumerated(EnumType.STRING)
-    private Gender gender;
+    private Gender gender; // MALE , male , Male , MalE
 
     @OneToOne
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private UserRole userRole;
+
+    @OneToMany(mappedBy = "teacher", cascade = CascadeType.REMOVE)
+    private List<StudentInfo> studentInfos;
+
+    @JsonIgnore
+    @ManyToMany
+    @JoinTable(
+            name = "user_lessonprogram",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "lesson_program_id")
+    )
+    private Set<LessonProgram> lessonsProgramList;
+
+    @JsonIgnore
+    @ManyToMany
+    @JoinTable(
+            name ="meet_student_table",
+            joinColumns = @JoinColumn(name = "student_id"),
+            inverseJoinColumns = @JoinColumn(name = "meet_id")
+    )
+    private List<Meet> meetList;
 
 }
---
+++ src/main/java/com/project/entity/enums/Note.java
@@ -0,0 +1,8 @@
+package com.project.entity.enums;
+
+public enum Note {
+
+    AA,BA,BB,CB,CC,DC,DD,DZ,FF
+
+
+}
